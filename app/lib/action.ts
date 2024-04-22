94c11340304b3c7c2755e0a1c44fcd50e87e2f5a
mengsuenyan <mengsuenyan@163.com>
2024-04-22 16:07:28 +0800

    Improving Accessibility
    - use `zod` to implement server-side form validation.
    - use the React useFormState hook to handle form errors, and display them to the user.

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -1,53 +1,87 @@
 'use server';
-import {z} from 'zod';
+import { z } from 'zod';
 import { insertInvoices, updateInvoice as dbUpdateInvoice, deleteInvoice as dbDeleteInvoice } from '@/app/lib/data';
 import { revalidatePath } from 'next/cache';
-import { notFound, redirect } from 'next/navigation';
+import { redirect } from 'next/navigation';
+import { data } from 'autoprefixer';
 
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({
+        invalid_type_error: 'Please select a customer',
+    }),
+    amount: z.coerce.number()
+        .gt(0, { message: 'Please enter an amount greater than $0.' })
+        .lt(100, { message: 'Please enter an amount lesser than $100.' }),
+    status: z.enum(['pending', 'paid'], {
+        invalid_type_error: 'Please select an invoice status'
+    }),
     date: z.string(),
 });
 
-const CreateInvoice = FormSchema.omit({id: true, date: true});
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
 
-const UpdateInvoice = FormSchema.omit({id: true, date: true});
+const UpdateInvoice = FormSchema.omit({ id: true, date: true });
 
-export async function createInvoice(formData: FormData) {
-    try {
-        const {customerId, amount, status} = CreateInvoice.parse({
-            customerId: formData.get('customerId'),
-            amount: formData.get('amount'),
-            status: formData.get('status'),
-        });
+// This is temporary until @types/react-dom is updated
+export type State = {
+    errors?: {
+        customerId?: string[];
+        amount?: string[];
+        status?: string[];
+    };
+    message?: string | null;
+};
 
-        const amountInCents = amount * 100;
-        const date = new Date().toISOString().split('T')[0];
+export async function createInvoice(prevState: State, formData: FormData) {
+    // safeParse() will return an object containing either a success or error field. This will help handle validation more gracefully without having put this logic inside the try/catch block.
+    const validatedFields = CreateInvoice.safeParse({
+        customerId: formData.get('customerId'),
+        amount: formData.get('amount'),
+        status: formData.get('status'),
+    });
 
-        await insertInvoices([{customer_id: customerId, amount: amountInCents, status, date}]);
-    } catch {
-        notFound();
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
     }
+
+    const {customerId, amount, status} = validatedFields.data;
+
+    const amountInCents = amount * 100;
+    const date = new Date().toISOString().split('T')[0];
+
+    await insertInvoices([{ customer_id: customerId, amount: amountInCents, status, date }]);
 
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
 
-export async function updateInvoice(id: string, formData: FormData) {
-    const { customerId, amount, status } = UpdateInvoice.parse({
+export async function updateInvoice(id: string, prevState: State, formData: FormData) {
+    const validatedFields = UpdateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
-      });
-     
+    });
+
+    // If form validation fails, return errors early. Otherwise, continue.
+    if (!validatedFields.success) {
+        return {
+            errors: validatedFields.error.flatten().fieldErrors,
+            message: 'Missing Fields. Failed to Create Invoice.',
+        };
+    }
+
+    const { customerId, amount, status } = validatedFields.data;
+
     const amountInCents = amount * 100;
 
-    await dbUpdateInvoice({id, customer_id: customerId, amount: amountInCents, status});
-     
+    await dbUpdateInvoice({ id, customer_id: customerId, amount: amountInCents, status });
+
     revalidatePath('/dashboard/invoices');
     redirect('/dashboard/invoices');
 }
